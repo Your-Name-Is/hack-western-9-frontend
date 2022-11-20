427a4a43c518ecf33d71b23321d465cb08362871
Henry Chen <henry@dyn172-30-52-164.wireless.uwo.pri>
2022-11-20 05:57:06 -0500

    Created Connection component

--- src/Connection.js
+++ src/Connection.js
@@ -1,7 +1,37 @@
-function Connection() {
-  return (
-    <p>Hello</p>
-  );
+import { useState } from 'react';
+
+function Connection(dataBase) {
+
+    const [firstName, setFirstName] = useState('User');
+    const [lastName, setLastName] = useState('Name');
+    const [position, setPosition] = useState('Position');
+    const [image, setImage] = useState(<img src="" alt="image"></img>); 
+    const [interest, setInterest] = ''; //list
+
+    function returnButton(){
+        console.log()
+    }
+
+return(
+    <div className="Connection">
+        <button onClick={returnButton()}><img src="" alt="return button"></img></button>
+        <div className="content">
+            {dataBase.map((profile) => (
+                <div className="Profile" key={dataBase.id}>
+
+                    <img src="" alt="image"></img>
+                    <h2>setfirstName{dataBase.firstName + " " + dataBase.lastName}</h2>
+                    <p></p>
+                    <h3>{dataBase.position}</h3>
+                    <p></p>
+                    <h4>{dataBase.interest}</h4>
+
+
+                </div>
+            ))}
+        </div>
+    </div>
+);
 }
 
 export default Connection;
