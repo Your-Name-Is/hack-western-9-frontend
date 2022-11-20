85ced9855578c227ed388c1be3515c74cd5f5b49
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2022-11-19 23:01:19 -0500

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import './App.css';
 
-import React from "react";
-import Webcam from "react-webcam";
-import { Container, Row, Col } from "react-bootstrap";
+import Home from "./Home";
 
 function App() {
-  const videoConstraints = {
-    facingMode: { exact: "environment" },
-    width: 2340,
-    height: 1080
-  };
-  const webcamRef = React.useRef(null);
-  const capture = React.useCallback(
-    () => {
-      const imageSrc = webcamRef.current.getScreenshot();
-    },
-    [webcamRef]
-  );
-
-  return (
-    <Container className="App">
-      <Webcam
-        className="stream"
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
-      />
-
-      <Row>
-        <Col className="top-button-container-1">
-          <Row className="top-button-container-2">
-            <button className="small-button">
-              <i class="bi bi-gear-fill"></i>
-            </button>
-          </Row>
-          <Row className="top-button-container-2">
-            <button className="small-button">
-              <i class="bi bi-person-fill"></i>
-            </button>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="bottom-button-container-1">
-        <button className="capture-button" onClick={capture}>
-          <i className="bi bi-person-bounding-box"></i>
-        </button>
-      </Row>
-    </Container>
+  return(
+    <Home />
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Webcam from "react-webcam";
+import { Container, Row } from "react-bootstrap";
+
+function Home() {
+  const videoConstraints = {
+    // facingMode: { exact: "environment" },
+    width: 2340,
+    height: 1080
+  };
+  const webcamRef = React.useRef(null);
+  const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+    },
+    [webcamRef]
+  );
+
+  return (
+    <Container className="App">
+      <Webcam
+        className="stream"
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+      />
+
+      <Row className="top-button-container-1">
+        <button className="small-button">
+          <i className="bi bi-gear-fill"></i>
+        </button>
+      </Row>
+      <Row className="bottom-button-container-1">
+        <button className="capture-button" onClick={capture}>
+          <i className="bi bi-person-bounding-box"></i>
+        </button>
+      </Row>
+    </Container>
+  );
+}
+
+export default Home;
