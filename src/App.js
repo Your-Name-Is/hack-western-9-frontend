48be7990c0a2c7103c202884175d75d9b02a1b6b
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2022-11-19 21:40:53 -0500

    Create front end

--- src/App.js
+++ src/App.js
@@ -2,16 +2,52 @@ import './App.css';
 
 import React from "react";
 import Webcam from "react-webcam";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   const videoConstraints = {
-    facingMode: { exact: "environment" }
+    facingMode: { exact: "environment" },
+    width: 2340,
+    height: 1080
   };
-  
+  const webcamRef = React.useRef(null);
+  const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+    },
+    [webcamRef]
+  );
+
   return (
-    <div className="App">
-      <Webcam videoConstraints={videoConstraints} />
-    </div>
+    <Container className="App">
+      <Webcam
+        className="stream"
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+      />
+
+      <Row>
+        <Col className="top-button-container-1">
+          <Row className="top-button-container-2">
+            <button className="small-button">
+              <i class="bi bi-gear-fill"></i>
+            </button>
+          </Row>
+          <Row className="top-button-container-2">
+            <button className="small-button">
+              <i class="bi bi-person-fill"></i>
+            </button>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="bottom-button-container-1">
+        <button className="capture-button" onClick={capture}>
+          <i className="bi bi-person-bounding-box"></i>
+        </button>
+      </Row>
+    </Container>
   );
 }
 
